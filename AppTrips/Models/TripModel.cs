a7fb735958ec4ecb26dbd4b713d94d8fdba15962
Eduardo Mendoza <eduardomendo17@hotmail.com>
2020-02-25 20:40:01 -0600

    Clase 25Feb20

--- AppTrips/Models/TripModel.cs
+++ AppTrips/Models/TripModel.cs
@@ -6,11 +6,13 @@ namespace AppTrips.Models
 {
     public class TripModel
     {
+        public int ID { get; set; }
         public string Title { get; set; }
         public DateTime TripDate { get; set; }
         public string Notes { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public int Rating { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
--- AppTrips/ViewModels/TripDetailViewModel.cs
+++ AppTrips/ViewModels/TripDetailViewModel.cs
@@ -2,16 +2,72 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AppTrips.ViewModels
 {
     public class TripDetailViewModel : BaseViewModel
     {
+        private int id;
+
+        Command _saveCommand;
+        public Command SaveCommand => _saveCommand ?? (_saveCommand = new Command(SaveAction));
+        /*public Command SaveCommand 
+        {
+            get
+            {
+                if (_saveCommand != null) return new Command(SaveAction);
+                return _saveCommand;
+            }
+        }*/
+
         string _Title;
         public string Title 
         { 
             get => _Title;
             set => SetProperty(ref _Title, value); 
+        }
+
+        string _Notes;
+        public string Notes
+        {
+            get => _Notes;
+            set => SetProperty(ref _Notes, value);
+        }
+
+        DateTime _TripDate;
+        public DateTime TripDate
+        {
+            get => _TripDate;
+            set => SetProperty(ref _TripDate, value);
+        }
+
+        double _Latitude;
+        public double Latitude
+        {
+            get => _Latitude;
+            set => SetProperty(ref _Latitude, value);
+        }
+
+        double _Longitude;
+        public double Longitude
+        {
+            get => _Longitude;
+            set => SetProperty(ref _Longitude, value);
+        }
+
+        int _Rating;
+        public int Rating
+        {
+            get => _Rating;
+            set => SetProperty(ref _Rating, value);
+        }
+
+        string _ImageUrl;
+        public string ImageUrl
+        {
+            get => _ImageUrl;
+            set => SetProperty(ref _ImageUrl, value);
         }
 
         public TripDetailViewModel()
@@ -20,7 +76,47 @@ namespace AppTrips.ViewModels
 
         public TripDetailViewModel(TripModel trip)
         {
+            id = trip.ID;
             Title = trip.Title;
+            Notes = trip.Notes;
+            Latitude = trip.Latitude;
+            Longitude = trip.Longitude;
+            Rating = trip.Rating;
+            TripDate = trip.TripDate;
+            ImageUrl = trip.ImageUrl;
+        }
+
+        private void SaveAction()
+        {
+            if (id == 0)
+            {
+                TripsViewModel.GetInstance().AddNewTrip(new TripModel
+                {
+                    Title = this.Title,
+                    Notes = this.Notes,
+                    Latitude = this.Latitude,
+                    Longitude = Longitude,
+                    Rating = Rating,
+                    TripDate = TripDate,
+                    ImageUrl = ImageUrl
+                });
+            }
+            else
+            {
+                TripsViewModel.GetInstance().ModifyTrip(new TripModel
+                {
+                    ID = id,
+                    Title = this.Title,
+                    Notes = this.Notes,
+                    Latitude = this.Latitude,
+                    Longitude = Longitude,
+                    Rating = Rating,
+                    TripDate = TripDate,
+                    ImageUrl = ImageUrl
+                });
+            }
+
+            Application.Current.MainPage.Navigation.PopAsync();
         }
     }
 }
